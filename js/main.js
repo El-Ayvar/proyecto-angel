846e7d662d8a7faed46e6566f7ff188d36798def
El-Ayvar <=>
2025-06-17 23:09:39 -0600

    medio telefono

---
+++ js/main.js
@@ -0,0 +1,60 @@
+
+function toggleMenu() {
+  const menu = document.getElementById('sideMenu');
+  menu.classList.toggle('active');
+}
+
+function closeMenu() {
+  const menu = document.getElementById('sideMenu');
+  menu.classList.remove('active');
+}
+
+ScrollReveal().reveal('.reveal_fade', {
+  duration: 2000,
+  origin: 'rigth',
+  distance: '20px',
+  opacity: 0,
+  easing: 'ease-out',
+  delay: 100,
+  reset: false
+});
+
+ScrollReveal().reveal('.reveal_slide', {
+  duration: 2000,
+  origin: 'left',
+  distance: '50px',
+  opacity: 0,
+  easing: 'ease-in-out',
+  delay: 200,
+  reset: false
+});
+
+ScrollReveal().reveal('.subtitulo_fade', {
+  duration: 2000,
+  origin: 'rigth',
+  distance: '20px',
+  opacity: 0,
+  easing: 'ease-out',
+  delay: 100,
+  reset: false
+});
+
+ScrollReveal().reveal('.subtitulo_slide', {
+  duration: 2000,
+  origin: 'left',
+  distance: '50px',
+  opacity: 0,
+  easing: 'ease-in-out',
+  delay: 200,
+  reset: false
+});
+
+ScrollReveal().reveal('.text_reveal', {
+  duration: 2000,
+  origin: 'right',
+  distance: '50px',
+  opacity: 0,
+  easing: 'ease-in-out',
+  delay: 200,
+  reset: false
+});
