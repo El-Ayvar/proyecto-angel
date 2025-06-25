7a3e327fc1162ea29293804674bb82fbbb9ff4a1
El-Ayvar <=>
2025-06-24 20:13:07 -0600

    Terminando fotter, y agregando 2 tarjetas mas

--- js/main.js
+++ js/main.js
@@ -48,23 +48,3 @@ ScrollReveal().reveal('.subtitulo_slide', {
   delay: 200,
   reset: false
 });
-
-ScrollReveal().reveal('.text_reveal', {
-  duration: 2000,
-  origin: 'right',
-  distance: '50px',
-  opacity: 0,
-  easing: 'ease-in-out',
-  delay: 200,
-  reset: false
-});
-
-ScrollReveal().reveal('.text_slide', {
-  duration: 2000,
-  origin: 'left',
-  distance: '50px',
-  opacity: 0,
-  easing: 'ease-in-out',
-  delay: 200,
-  reset: false
-});
